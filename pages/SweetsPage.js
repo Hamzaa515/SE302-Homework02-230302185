28ec4b382a1a63f48e4ab7efe15e0bf0630eda26
Hamzaa515 <fejzichamza91@gmail.com>
2025-12-29 01:40:32 +0100

    Update SweetsPage.js

--- pages/SweetsPage.js
+++ pages/SweetsPage.js
@@ -1,26 +1,24 @@
 class SweetsPage {
   constructor(page) {
     this.page = page;
-    this.addButtons = page.locator('button', { hasText: 'Add to Basket' });
+    this.products = page.locator('.card');
+    this.firstAddButton = this.products.first().locator('button');
   }
 
   async open() {
-    await this.page.goto('/sweets', { waitUntil: 'domcontentloaded' });
-    await this.page.waitForLoadState('networkidle');
-    await this.addButtons.first().waitFor({ state: 'visible' });
+    await this.page.goto('/sweets');
+    await this.products.first().waitFor({ state: 'visible' });
   }
 
   async addFirstItemToBasket() {
     await this.open();
-    await this.addButtons.first().click();
+    await this.firstAddButton.click();
   }
 
   async addButtonCount() {
-    return await this.addButtons.count();
+    return await this.page.locator('.card button').count();
   }
 }
 
 module.exports = SweetsPage;
-
-
 
